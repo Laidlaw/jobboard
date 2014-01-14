d0de1c75496322336a9326fa06ed79a4bdaee1a9
Alan Laidlaw <alanlaidlaw@gmail.com>
2014-01-14 04:23:51 -0600

    set up environment. added jobs.json to get he ball rolling

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('jobboardApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.url = 'jobs.json';
+    $scope.jobs = [];
+
+    $scope.fetchJobs = function() {
+      $http.get($scope.url).then(function(result){
+        $scope.jobs = result.data;
+      });
+    };
+    $scope.fetchJobs();
+
+
+  });
